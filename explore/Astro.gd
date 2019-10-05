73921373e87d354f1ce396a2d7c399f6d0328552
Garrett Allen <gigpop21@gmail.com>
2019-10-04 20:05:25 -0400

    explore camera hooked

--- explore/Astro.gd
+++ explore/Astro.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 100
+const MAX_SPEED = 200
 const ACCEL = 10
+const DECEL = 8
 
 var velocity = Vector2(0,0)
 
@@ -11,19 +12,27 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.look_at(get_viewport().get_mouse_position())
+	self.look_at($Camera2D.get_global_mouse_position())
 	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += ACCEL
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x -= ACCEL
+	elif abs(velocity.x) > 0:
+		velocity.x = velocity.x - (DECEL * (velocity.x/abs(velocity.x)))
+		if abs(velocity.x) <= DECEL:
+			velocity.x = 0
 		
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= ACCEL
 	elif Input.is_action_pressed("ui_down"):
 		velocity.y += ACCEL
+	elif abs(velocity.y) > 0:
+		velocity.y = velocity.y - (DECEL * (velocity.y/abs(velocity.y)))
+		if abs(velocity.y) <= DECEL:
+			velocity.y = 0
 		
-	if velocity.x + velocity.y > MAX_SPEED:
+	if velocity.length() > MAX_SPEED:
 		velocity = velocity.normalized()
 		velocity *= MAX_SPEED
 		
