8ede947be7d9331cdcca363caed4fd1474e91106
Garrett Allen <gigpop21@gmail.com>
2019-10-04 21:21:12 -0400

    Some gravity and orbits woo

--- launch/Planet1.gd
+++ launch/Planet1.gd
@@ -1,13 +1,36 @@
 extends StaticBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var gravity = 120
+export var planetRadius = 32
+
+var gravityPullingObjects = []
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$planet1.transform.x = Vector2(planetRadius/32, 0)
+	$planet1.transform.y = Vector2(0, planetRadius/32)
+	
+	var planetShape = CircleShape2D.new()
+	planetShape.set_radius(planetRadius)
+	$CollisionShape2D.set_shape(planetShape)
+
+	var gravityShape = CircleShape2D.new()
+	gravityShape.set_radius(planetRadius*5)
+	$GravityArea2D/CollisionShape2D.set_shape(gravityShape)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	for obj in gravityPullingObjects:
+		var gravityPull = obj.global_position.direction_to(self.global_position) * gravity * delta
+		obj.gravityVelocity += gravityPull
+
+
+func _on_GravityArea2D_body_entered(body):
+	if body.is_in_group("ship"):
+		gravityPullingObjects.append(body)
+	
+
+
+func _on_GravityArea2D_body_exited(body):
+	gravityPullingObjects.erase(body)
--- launch/ShipLaunching.gd
+++ launch/ShipLaunching.gd
@@ -1,20 +1,33 @@
 extends KinematicBody2D
 
-const THRUST_POWER = 100
+const THRUST_POWER = 135
+
+const MAX_SPEED = 300
 
 export var velocity = Vector2(0,0)
+var gravityVelocity = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(delta)
 	if Input.is_action_pressed("mouse_left"):
 		var thrustDir = global_position.direction_to($Camera2D.get_global_mouse_position())
 		var thrust = thrustDir.normalized()*THRUST_POWER*delta
 		velocity += thrust
-	move_and_slide(velocity)
+	
+	
+	self.rotation = velocity.angle()
+	
+	if velocity.length() > MAX_SPEED:
+		velocity = velocity.normalized() * MAX_SPEED
+		
+	if gravityVelocity.length() > MAX_SPEED:
+		gravityVelocity = gravityVelocity.normalized() * MAX_SPEED
+		
+	move_and_slide(velocity + gravityVelocity)
+	
 	
 		
